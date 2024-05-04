2ca00c74f11395488b26667ec0fd44506f636056
dpdd <dpdd@squat.net>
2024-05-04 11:58:45 +0200

    Update quiz.vue and quizStore.ts to improve quiz functionality

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 useProvideQuizStore()
+
+const route = useRoute()
 </script>
 
 <template>
   <div class="h-[100dvh] flex flex-col bg-slate-950 flex-shrink">
     <QuizHeader />
     <div class="flex flex-grow p-4 items-center justify-center">
-      <router-view />
+      <router-view :key="route.fullPath" />
     </div>
   </div>
 </template>
--- utils/quizStore.ts
+++ utils/quizStore.ts
@@ -1,6 +1,24 @@
-const [useProvideQuizStore, useQuizStore] = createInjectionState(() => {
+const [useProvideQuizStore, useQuizStore] = createInjectionState((): {
+  score: Ref<number>
+  questions: Ref<QuizQuestion[] | null>
+  scorePercentage: ComputedRef<number>
+  scorePercentageRounded: ComputedRef<number>
+} => {
   const score = ref(0)
-  return { score }
+
+  const { data: questions } = useAsyncData('questions', async () => {
+    return await queryContent<QuizQuestion>('quiz', 'questions').find()
+  })
+
+  const scorePercentage = computed<number>(() => {
+    if (!questions.value)
+      return 0
+    return (score.value / questions.value.length) * 100
+  })
+
+  const scorePercentageRounded = useRound(scorePercentage)
+
+  return { score, questions, scorePercentage, scorePercentageRounded }
 })
 
 export { useProvideQuizStore, useQuizStore }
